7c7344797c4376d7e04e61240aeaea8ce6cdf106
Levan MARTIN <121493470+SpiizN@users.noreply.github.com>
2024-02-20 00:01:49 +0100

    Working Proxy
    - Linux required for logging ("/var/log/levwall/proxy").
    - setup.sh in progress

---
+++ setup.sh
@@ -0,0 +1,7 @@
+mkdir /var/lib/levwall/
+mkdir /var/lib/levwall/proxy/
+cp proxy/proxy.py /var/lib/levwall/proxy/proxy.py
+cp proxy/blocked.csv /var/lib/levwall/proxy/blocked.csv
+
+mkdir /var/log/levwall/
+mkdir /var/log/levwall/proxy/
